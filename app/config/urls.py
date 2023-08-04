c2c988b60815a882be4ff5fa3a9b57ec38f14aa9
Nik Uzair <nikuzairsc@gmail.com>
2023-08-04 11:26:55 +0800

    Create core app for base pages

--- app/config/urls.py
+++ app/config/urls.py
@@ -16,9 +16,10 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
-from core.views import frontpage
+from core.views import frontpage, about
 
 urlpatterns = [
     path('', frontpage, name='frontpage'),
+    path('about/', about, name='about'),
     path('admin/', admin.site.urls),
 ]
--- app/core/views.py
+++ app/core/views.py
@@ -3,3 +3,7 @@ from django.shortcuts import render
 
 def frontpage(request):
     return render(request, 'core/frontpage.html')
+
+
+def about(request):
+    return render(request, 'core/about.html')
