fa45ae3f6752330a8b03fa2a73d9f4ec7f50d0b9
Nik Uzair <nikuzairsc@gmail.com>
2023-08-04 14:20:01 +0800

    Change function to class-based view

--- app/config/urls.py
+++ app/config/urls.py
@@ -15,12 +15,12 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import include, path
-
 from core.views import frontpage, about
 
 urlpatterns = [
     path('', include('store.urls')),
-    path('', frontpage, name='frontpage'),
+    # path('', frontpage, name='frontpage'),
+    path('', frontpage.as_view(), name='frontpage'),
     path('about/', about, name='about'),
     path('admin/', admin.site.urls),
 ]
--- app/core/views.py
+++ app/core/views.py
@@ -1,13 +1,17 @@
+from django.views.generic.list import ListView
 from django.shortcuts import render
-
 from store.models import Product
 
 
-def frontpage(request):
-    products = Product.objects.all()[0:6]
-    return render(request, 'core/frontpage.html', {
-        'products': products
-    })
+# def frontpage(request):
+#     products = Product.objects.all()[0:6]
+#     return render(request, 'core/frontpage.html', {
+#         'products': products
+#     })
+
+class frontpage(ListView):
+    """ Display all products """
+    model = Product
 
 
 def about(request):
