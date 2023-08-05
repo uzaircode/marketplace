f8c7d6c694e0150f6fc0adb3a9669cf65d1cf284
Nik Uzair <nikuzairsc@gmail.com>
2023-08-06 06:40:27 +0800

    Add previous function-based add_profile
    - commenting out the class-based view, and use the previous one instead

--- app/userprofile/urls.py
+++ app/userprofile/urls.py
@@ -3,7 +3,7 @@ from django.urls import path
 
 from . import views
 
-from .views import AddProduct
+# from .views import AddProduct
 
 urlpatterns = [
     path('signup/', views.signup, name='signup'),
@@ -12,7 +12,8 @@ urlpatterns = [
     path("logout/", auth_views.LogoutView.as_view(), name="logout"),
     path("myaccount/", views.myaccount, name="myaccount"),
     path('my-store/', views.my_store, name='my_store'),
-    path('my-store/add-product/', AddProduct.as_view(), name='add_product'),
+    path('my-store/add-product/', views.add_product, name='add_product'),
+    # path('my-store/add-product/', AddProduct.as_view(), name='add_product'),
     path('my-store/edit-product/<int:pk>/',
          views.edit_product, name='edit_product'),
     path('vendors/<int:pk>/', views.vendor_detail, name='vendor_detail'),
