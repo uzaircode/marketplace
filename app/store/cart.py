9a9dfeaf3f42a1560447dbff3c609977928a52a8
Nik Uzair <nikuzairsc@gmail.com>
2023-08-06 10:58:57 +0800

    Add increment/decrement products in the cart

--- app/store/cart.py
+++ app/store/cart.py
@@ -59,5 +59,4 @@ class Cart(object):
         for p in self.cart.keys():
             self.cart[str(p)]['product'] = Product.objects.get(pk=p)
 
-        return sum(item['product'].price * item['quantity']
-                   for item in self.cart.values()) / 100
+        return int(sum(item['product'].price * item['quantity'] for item in self.cart.values()))
--- app/store/urls.py
+++ app/store/urls.py
@@ -6,6 +6,8 @@ urlpatterns = [
     path('search/', views.search, name='search'),
     path('add-to-cart/<int:product_id>/',
          views.add_to_cart, name='add_to_cart'),
+    path('remove-from-cart/<str:product_id>',
+         views.change_quantity, name='change_quantity'),
     path('remove-from-cart/<str:product_id>/',
          views.remove_from_cart, name='remove_from_cart'),
     path('cart/', views.cart_view, name='cart_view'),
