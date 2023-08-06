460642e068a7158bd88d3d6398294f6dcd53127d
Nik Uzair <nikuzairsc@gmail.com>
2023-08-06 13:53:55 +0800

    Add cart globally
    - Add cart button globally available

--- app/userprofile/models.py
+++ app/userprofile/models.py
@@ -5,6 +5,7 @@ from django.db import models
 class UserProfile(models.Model):
     user = models.OneToOneField(
         User, related_name='userprofile', on_delete=models.CASCADE)
+    is_vendor = models.BooleanField(default=False)
 
     def __str__(self):
         return self.user.username
